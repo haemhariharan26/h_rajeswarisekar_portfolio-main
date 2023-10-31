d48f8a249d187aee9a6bfb9de9ad7c385aae0d91
haemhariharan26 <113370966+haemhariharan26@users.noreply.github.com>
2023-10-31 19:59:36 -0400

    change made

---
+++ js/main.js
@@ -0,0 +1,26 @@
+let anonFunction = function() {
+    console.log("anon function called");
+}
+
+anonFunction();
+
+let anonFunction2 = function() {
+    console.log("anon2 function called");
+}();
+
+
+// IIFEE regular syntax
+(function() {
+    console.log("IIFE called");
+    const userName = "Marco";
+    console.log(userName);
+})();
+
+
+
+
+
+// arrow syntax
+(() =>  {
+    console.log("arrow iffe called");
+})();
